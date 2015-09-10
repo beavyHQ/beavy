b3f543c1abaed70ad0fe46e27b6fa87f710c1a6a
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-10 13:44:01 +0200

    add yaml-based environments and config

--- beavy/app.py
+++ beavy/app.py
@@ -1,3 +1,11 @@
 from flask import Flask
+from flask_environments import Environments
+
+import os
 
 app = Flask(__name__)
+env = Environments(app, var_name="BEAVY_ENV", default_env="PRODUCTION")
+
+env.from_yaml(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yml'))
+env.from_yaml(os.path.join(os.getcwd(), 'config.yml'))
+
