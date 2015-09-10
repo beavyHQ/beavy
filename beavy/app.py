7b69ba5099865a8eac5c3cbb2dbaea3fa53eaa4a
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-10 22:38:12 +0200

    add first edition of flask migrate

--- beavy/app.py
+++ beavy/app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
 from flask_environments import Environments
 
 from .utils import load_modules
@@ -10,5 +14,11 @@ env = Environments(app, var_name="BEAVY_ENV")
 
 env.from_yaml(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yml'))
 env.from_yaml(os.path.join(os.getcwd(), 'config.yml'))
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 load_modules(app)
---
+++ manager.py
@@ -0,0 +1,5 @@
+from beavy.app import app, manager
+
+
+if __name__ == '__main__':
+    manager.run()
