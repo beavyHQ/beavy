49d5abe508bf9bbd25632eb977f1cf6d6829a693
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-11 12:14:10 +0200

    add polymorphic base object

--- beavy/models/__init__.py
+++ beavy/models/__init__.py
@@ -2,6 +2,7 @@ from beavy.app import db
 from flask.ext.security import SQLAlchemyUserDatastore, \
     UserMixin, RoleMixin
 
+from sqlalchemy.dialects.postgresql import JSONB
 
 # Define models
 roles_users = db.Table('roles_users',
@@ -24,3 +25,21 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+
+class Object(db.Model):
+    """
+    This is the primary base class for all kind of objects
+    we know of inside the system
+    """
+    __tablename__ = "objects"
+
+    id = db.Column(db.Integer, primary_key=True)
+    discriminator = db.Column('type', db.String(100))
+    created_at = db.Column('created_at', db.DateTime())
+    payload = db.Column('payload', JSONB)
+    owner = db.Column(db.Integer, db.ForeignKey(User.id))
+    belongs_to = db.Column(db.Integer, db.ForeignKey("objects.id"), nullable=True)
+    children = db.relationship("Object", backref=db.backref('belongs_to',
+                                                            remote_side=id))
+
+    __mapper_args__ = {'polymorphic_on': discriminator}
