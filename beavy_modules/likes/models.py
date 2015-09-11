d7b0155eac268b42e09796d69655957681234bee
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-11 19:25:14 +0200

    drop external likes in favor of new activity system

--- beavy_modules/likes/models.py
+++ beavy_modules/likes/models.py
@@ -1,14 +1,11 @@
 
 from beavy.app import db
-from beavy.models import User, Object
+from beavy.models.activity import Activity
+from beavy.models.object import Object
 
-import datetime
 
-
-class Like(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created = db.Column(db.DateTime())
-    user = db.relationship(User)
+class Like(Activity):
+    pass
 
 
 Object.likes_count = db.Column(db.Integer)
