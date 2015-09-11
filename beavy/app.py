5db5464c90f5fa5e4e9b466d4d9654722cec9245
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-11 17:33:12 +0200

    install marshmallow. upgrade requirements to latest requirements

--- beavy/app.py
+++ beavy/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext import migrate as ext_migrate
+from flask.ext.marshmallow import Marshmallow
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.security import Security, SQLAlchemyUserDatastore, \
     UserMixin, RoleMixin, login_required
@@ -14,18 +15,29 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# The app
 app = Flask(__name__)
+
+# environment-based configuration loading
 env = Environments(app, var_name="BEAVY_ENV")
 
 env.from_yaml(os.path.join(BASE_DIR, 'config.yml'))
 env.from_yaml(os.path.join(os.getcwd(), 'config.yml'))
 
 
+# start database
 db = SQLAlchemy(app)
+# and database migrations
 migrate = Migrate(app, db)
 
+# initialize Resource-Based API-System
+ma = Marshmallow(app)
+
+# scripts manager
 manager = Manager(app)
+# add DB+migrations commands
 manager.add_command('db', MigrateCommand)
+
 
 from beavy.models import User, Role
 
