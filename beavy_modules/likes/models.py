d180675cecd6021f4333f7ead410031ef35e0cab
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-11 12:14:26 +0200

    add object likes

--- beavy_modules/likes/models.py
+++ beavy_modules/likes/models.py
@@ -1,6 +1,6 @@
 
 from beavy.app import db
-from beavy.models import User
+from beavy.models import User, Object
 
 import datetime
 
@@ -9,3 +9,6 @@ class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created = db.Column(db.DateTime())
     user = db.relationship(User)
+
+
+Object.likes_count = db.Column(db.Integer)
