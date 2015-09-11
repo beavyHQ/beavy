50aecf7b63523353bfe0fe55f04dfa71554d07b4
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-11 15:05:32 +0200

    adding Activities infrastructure

--- beavy/models/__init__.py
+++ beavy/models/__init__.py
@@ -43,3 +43,20 @@ class Object(db.Model):
                                                             remote_side=id))
 
     __mapper_args__ = {'polymorphic_on': discriminator}
+
+
+class Activity(db.Model):
+    """
+    We record activities on objects through this
+    """
+    __tablename__ = "activities"
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.Integer, db.ForeignKey(User.id))
+    discriminator = db.Column('verb', db.String(100))
+    created_at = db.Column('created_at', db.DateTime())
+    object = db.Column(db.Integer, db.ForeignKey("objects.id"), nullable=True)
+    whom = db.Column(db.Integer, db.ForeignKey(User.id), nullable=True)
+    payload = db.Column('payload', JSONB)
+
+    __mapper_args__ = {'polymorphic_on': discriminator}
