3940b3abef279b2bbd719151fbb0895f652a642a
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-11 19:21:16 +0200

    add awesome automagically morphing schema field

---
+++ beavy/common/morphing_field.py
@@ -0,0 +1,37 @@
+from marshmallow.fields import Field
+
+
+class MorphingField(Field):
+
+    # registry = {
+    # }
+
+    def __init__(self, many=False, fallback=None, overwrite=None, **metadata):
+        self.many = False
+        self.fallback = fallback or self.FALLBACK
+        self.overwrite = overwrite
+
+    # Common alternative:
+    # def _obj_to_name(self, obj):
+    #     return obj.__class__.__name__
+
+    def _obj_to_name(self, obj):
+        return getattr(obj, obj.__mapper_args__["polymorphic_on"], None)
+
+    def _serialize(self, value):
+        if value is None:
+            return None
+        if self.many:
+            return [self._get_serializer(x).dump(x).data for x in value]
+        return self._get_serializer(value).dump(value).data
+
+    def _get_serializer(self, obj):
+        name = self._obj_to_name(obj)
+        if self.overwrite:
+            kls = self.overwrite(obj, name)
+            if isinstance(kls, str):
+                name = kls
+            elif callable(kls):
+                return kls()
+
+        return self.registry.get(name, self.baseObject)()
