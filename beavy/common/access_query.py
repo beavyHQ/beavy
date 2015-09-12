e531b7fe3fb14c587fe539567a581466a47d80d7
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-12 03:53:49 +0200

    Improved Access Query: returns nothing if no filter is found

--- beavy/common/access_query.py
+++ beavy/common/access_query.py
@@ -4,19 +4,23 @@ from sqlalchemy.sql import or_
 
 class AccessQuery(BaseQuery):
 
-    def _filter_for(self, class_, fltr):
-        return class_.query.filter(or_(
-                    *[x for x in filter(lambda x: x is not None, [
-                        t(class_, fltr)
-                        # weirdly class_.__access_filters fails on us
-                        # probably some sqlalchemy magic
-                        for t in getattr(class_, "__access_filters")[fltr]
-                    ])]))
+    def _gen_filters(self, class_, fltr):
+        return [x for x in filter(lambda x: x is not None, [
+            t(class_, fltr)
+            # weirdly class_.__access_filters fails on us
+            # probably some sqlalchemy magic
+            for t in getattr(class_, "__access_filters")[fltr]]
+            )
+        ]
 
     def filter_visible(self, attr, remoteAttr):
+        filters = self._gen_filters(remoteAttr.class_, 'view')
+        if not filters:
+            return self.filter(False)
+
         return self.filter(attr.in_(
-            self._filter_for(remoteAttr.class_, 'view')
-                .with_entities(remoteAttr)
-                .subquery()
+            remoteAttr.class_.query.filter(or_(*filters))
+                             .with_entities(remoteAttr)
+                             .subquery()
             )
         )
