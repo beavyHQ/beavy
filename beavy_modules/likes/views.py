a0bbd9a95ae645a4989c5ca9099d90bc80582779
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-13 21:42:36 +0200

    use new url converters

--- beavy_modules/likes/views.py
+++ beavy_modules/likes/views.py
@@ -7,13 +7,13 @@ from .models import Like
 from .schemas import user_likes_paged
 
 
-@blueprint.route("/u/<int:user_id>/likes")
+@blueprint.route("/u/<user:user>/likes")
 @fallbackRender('user_likes.html')
-def user_likes(user_id):
-    print(user_id)
+def user_likes(user):
+    print(user)
     return user_likes_paged.dump(as_page(
         Like.query
-            .filter(Like.subject_id == user_id)
+            .filter(Like.subject_id == user.id)
             .filter_visible(Like.object_id, Object.id)
             .options(subqueryload(Like.object)),
         error_out=False))
