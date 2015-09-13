95bbd995b5ae8dcefa5b51f450be4dffb74763c4
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-13 13:02:17 +0200

    switch on user tracking

--- beavy/models/user.py
+++ beavy/models/user.py
@@ -16,5 +16,12 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
+
+    last_login_at = db.Column(db.DateTime())
+    current_login_at = db.Column(db.DateTime())
+    last_login_ip = db.Column(db.String(255))
+    current_login_ip = db.Column(db.String(255))
+    login_count = db.Column(db.Integer())
+
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
