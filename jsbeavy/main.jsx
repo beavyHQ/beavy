2e35bb3f815c59dbae8014c2c74662997575aa39
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-13 11:05:42 +0200

    remove wrong escaping

--- jsbeavy/main.jsx
+++ jsbeavy/main.jsx
@@ -6,7 +6,7 @@ import makeRoutes from "config/routes";
 import stores from "config/mainStores";
 import renderApplication from "config/renderApplication";
 
-let Application = require("module-imports?ext=/\application.jsx&key=FRONTEND&path=config/apps/!yaml!../config.yml").default;
+let Application = require("module-imports?ext=/application.jsx&key=FRONTEND&path=config/apps/!yaml!../config.yml").default;
 
 renderApplication(makeRoutes(Application), stores, {
   timeout: 600
