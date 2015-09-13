30b953b4ae13b246624f0714cde5a32c184a4f38
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-13 13:03:15 +0200

    separate out styles

--- jsbeavy/webpack-dev-server.config.js
+++ jsbeavy/webpack-dev-server.config.js
@@ -1,5 +1,6 @@
 module.exports = require("./make-webpack-config")({
 	devServer: true,
 	devtool: "eval",
+  separateStylesheet: true,
 	debug: true
 });
--- jsbeavy/webpack-hot-dev-server.config.js
+++ jsbeavy/webpack-hot-dev-server.config.js
@@ -2,5 +2,6 @@ module.exports = require("./make-webpack-config")({
 	devServer: true,
 	hotComponents: true,
 	devtool: "source-map",
+  separateStylesheet: true,
 	debug: true
 });
