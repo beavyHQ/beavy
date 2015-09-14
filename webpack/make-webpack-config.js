f6ee8330a26ee333b5b03e9d24a6994ce02c905d
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-14 16:14:32 +0200

    clean up webpack: make it its own directory

--- webpack/make-webpack-config.js
+++ webpack/make-webpack-config.js
@@ -4,8 +4,9 @@ var yaml = require('js-yaml');
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var StatsPlugin = require("stats-webpack-plugin");
-var loadersByExtension = require("./config/loadersByExtension");
+var loadersByExtension = require("./helpers/loadersByExtension");
 var appConfig = yaml.safeLoad(fs.readFileSync('config.yml'));
+var ROOT = path.join(__dirname, "..", "jsbeavy");
 
 module.exports = function(options) {
 	var entry = {
@@ -18,7 +19,7 @@ module.exports = function(options) {
 		"jsx": options.hotComponents ? ["react-hot-loader", "babel-loader?stage=0"] : "babel-loader?stage=0",
 		"js": {
 			loader: "babel-loader?stage=0",
-			include: __dirname
+			include: ROOT
 		},
 		"json": "json-loader",
 		"coffee": "coffee-redux-loader",
@@ -38,11 +39,11 @@ module.exports = function(options) {
 		"styl": [cssLoader, "stylus-loader"],
 		"scss|sass": [cssLoader, "sass-loader" +
 			// highest priority: __CUSTOM/styles
-			"?includePaths[]=" + encodeURIComponent(path.resolve(__dirname, "__CUSTOM", "styles")) +
+			"?includePaths[]=" + encodeURIComponent(path.resolve(ROOT, "__CUSTOM", "styles")) +
 			// second: App
-			"&includePaths[]=" + encodeURIComponent(path.resolve(__dirname, "config", "apps", appConfig.FRONTEND, "styles")) +
+			"&includePaths[]=" + encodeURIComponent(path.resolve(ROOT, "config", "apps", appConfig.FRONTEND, "styles")) +
 			// third: defaults
-			"&includePaths[]=" + encodeURIComponent(path.resolve(__dirname, "styles"))]
+			"&includePaths[]=" + encodeURIComponent(path.resolve(ROOT, "styles"))]
 	};
 	var additionalLoaders = [
 		// { test: /some-reg-exp$/, loader: "any-loader" }
@@ -58,7 +59,7 @@ module.exports = function(options) {
 	];
 	var modulesDirectories = ["web_modules", "node_modules"];
 	var extensions = ["", ".web.js", ".js", ".jsx"];
-	var root = __dirname;
+	var root = ROOT;
 	var publicPath = options.devServer ?
 		"http://localhost:2992/_assets/" :
 		"/_assets/";
@@ -77,7 +78,10 @@ module.exports = function(options) {
 	];
 	var plugins = [
 		new webpack.PrefetchPlugin("react"),
-		new webpack.PrefetchPlugin("react/lib/ReactComponentBrowserEnvironment")
+		new webpack.PrefetchPlugin("react/lib/ReactComponentBrowserEnvironment"),
+		new webpack.DefinePlugin({
+				__DEBUG__    : !!options.debug,
+  			__DEBUG_NW__ : !!options.nw}),
 	];
 	if(options.prerender) {
 		plugins.push(new StatsPlugin(path.join(__dirname, "build", "stats.prerender.json"), {
@@ -102,14 +106,6 @@ module.exports = function(options) {
 	if(options.commonsChunk) {
 		plugins.push(new webpack.optimize.CommonsChunkPlugin("commons", "commons.js" + (options.longTermCaching && !options.prerender ? "?[chunkhash]" : "")));
 	}
-	var asyncLoader = {
-		test: require("./route-handlers/async").map(function(name) {
-			return path.join(__dirname, "app", "route-handlers", name);
-		}),
-		loader: options.prerender ? "react-proxy-loader/unavailable" : "react-proxy-loader"
-	};
-
-
 
 	Object.keys(stylesheetLoaders).forEach(function(ext) {
 		var stylesheetLoader = stylesheetLoaders[ext];
@@ -151,7 +147,7 @@ module.exports = function(options) {
 		output: output,
 		target: options.prerender ? "node" : "web",
 		module: {
-			loaders: [asyncLoader].concat(loadersByExtension(loaders)).concat(loadersByExtension(stylesheetLoaders)).concat(additionalLoaders)
+			loaders: [].concat(loadersByExtension(loaders)).concat(loadersByExtension(stylesheetLoaders)).concat(additionalLoaders)
 		},
 		devtool: options.devtool,
 		debug: options.debug,
