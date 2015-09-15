72a90e2aef70e236616742106ed9383481058d9a
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-16 00:41:14 +0200

    automagically update likes count on object liked upon

--- beavy_modules/likes/models.py
+++ beavy_modules/likes/models.py
@@ -1,6 +1,7 @@
 from beavy.app import db
 from beavy.models.activity import Activity
 from beavy.models.object import Object
+from sqlalchemy import event
 
 
 class Like(Activity):
@@ -9,4 +10,12 @@ class Like(Activity):
     }
 
 
-Object.likes_count = db.Column(db.Integer)
+Object.likes_count = db.Column(db.Integer())
+
+
+@event.listens_for(Like, 'after_insert')
+def update_object_likes_count(mapper, connection, target):
+    # unfortunately, we can't do an aggregate in update directly...
+    likes_count = Like.query.filter(Like.object_id == target.object_id).count()
+    Object.query.filter(Object.id == target.object_id
+                        ).update({'likes_count': likes_count})
