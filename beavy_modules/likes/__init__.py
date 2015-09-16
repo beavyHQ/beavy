70c526b2300def366774d8e4b71fa216785d9190
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-16 11:02:25 +0200

    Better Patching of other Objects from Like
    using the improved system for init_app, this can now patch all object-field
    models without having to explictly import each one.

--- beavy_modules/likes/__init__.py
+++ beavy_modules/likes/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from beavy.schemas.object import ObjectField, fields
 
 blueprint = Blueprint('likes', __name__,
                       template_folder='templates')
@@ -7,5 +8,9 @@ from .views import *
 
 
 def init_app(app):
+    # patch all objects to have likes_counts when serialized
+    for cls in ObjectField.registry.values():
+        cls._declared_fields['likes_count'] = fields.Integer(default=0)
+
     # tell flask about our own templates
     app.register_blueprint(blueprint)
--- beavy_modules/likes/schemas.py
+++ beavy_modules/likes/schemas.py
@@ -1,8 +1,7 @@
 from beavy.common.paging_schema import makePaginationSchema
 from beavy.schemas.user import BaseUser
 from beavy.schemas.activity import ActivityField
-from beavy.schemas.object import ObjectField, BaseObject
-from beavy.app import app
+from beavy.schemas.object import ObjectField
 from marshmallow import Schema, fields
 
 
@@ -11,15 +10,6 @@ class BaseLike(Schema):
     created_at = fields.DateTime()
 
 ActivityField.registry['Like'] = BaseLike
-
-# Patching the base class to include likes_count
-# internal marshmallow things...
-BaseObject._declared_fields['likes_count'] = fields.Integer(default=0)
-
-# FIXME: there must be a nicer way to manage this integration
-if 'comments' in app.config.get("MODULES"):
-    from beavy_modules.comments.models import CommentSchema
-    CommentSchema._declared_fields['likes_count'] = fields.Integer(default=0)
 
 
 class UserLike(Schema):
