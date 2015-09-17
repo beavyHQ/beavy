05132020ef54afc67580a32a74cfea4d9e8052ff
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-17 18:27:21 +0200

    Remove print

--- beavy_modules/likes/views.py
+++ beavy_modules/likes/views.py
@@ -10,7 +10,6 @@ from .schemas import user_likes_paged
 @users_bp.route("/<user:user>/likes/")
 @fallbackRender('user_likes.html')
 def user_likes(user):
-    print(user)
     return user_likes_paged.dump(as_page(
         Like.query
             .filter(Like.subject_id == user.id)
