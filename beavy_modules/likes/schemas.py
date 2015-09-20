75b7d9311ef9cf9fbcd4b959b0acb5d1876e2a53
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-20 17:43:45 +0200

    use new jsonapi

--- beavy_modules/likes/schemas.py
+++ beavy_modules/likes/schemas.py
@@ -2,7 +2,9 @@ from beavy.common.paging_schema import makePaginationSchema
 from beavy.schemas.user import BaseUser
 from beavy.schemas.activity import ActivityField
 from beavy.schemas.object import ObjectField
-from marshmallow import Schema, fields
+# from marshmallow import Schema, fields
+
+from marshmallow_jsonapi import Schema, fields
 
 
 class BaseLike(Schema):
@@ -15,6 +17,14 @@ ActivityField.registry['Like'] = BaseLike
 class UserLike(Schema):
     created_at = fields.DateTime()
     object_id = fields.Integer()
+
+    class Meta:
+        type_ = "like"
+
+    object = fields.HyperlinkRelated(
+        '/authors/{author_id}',
+        url_kwargs={'object_id': '<author.id>'},
+    )
     object = ObjectField()
 
 
