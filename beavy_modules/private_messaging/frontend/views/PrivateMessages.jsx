4155ea007638aa00a5972c952c211dbd09d8cdc8
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-21 14:23:40 +0200

    pretty inifinite scrolling for pagination

--- beavy_modules/private_messaging/frontend/views/PrivateMessages.jsx
+++ beavy_modules/private_messaging/frontend/views/PrivateMessages.jsx
@@ -2,62 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { loadPMs } from '../actions';
 import { PRIVATE_MESSAGES } from '../reducers';
-// import CloakingList from 'components/CloakingList';
-
-function topPosition(domElt) {
-  if (!domElt) {
-    return 0;
-  }
-  return domElt.offsetTop + topPosition(domElt.offsetParent);
-}
-
-class InfiniteScrollingList extends Component{
-
-  getDefaultProps() {
-    return {
-      hasMore: false,
-      loadMore() {},
-      threshold: 250
-    };
-  }
-  componentDidMount() {
-    this.attachScrollListener();
-  }
-  componentDidUpdate() {
-    this.attachScrollListener();
-  }
-  render() {
-    var props = this.props;
-    return <div ref='main'>
-            {props.children}
-           </div>;
-  }
-  scrollListener() {
-    var el = React.findDOMNode(this.refs.main);
-    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    if ((topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight) < Number(this.props.threshold)) {
-      this.detachScrollListener();
-      // call loadMore after detachScrollListener to allow
-      // for non-async loadMore functions
-      !this.props.loading && this.props.loadMore();
-    }
-  }
-  attachScrollListener() {
-    if (!this.props.hasMore) {
-      return;
-    }
-    window.addEventListener('scroll', ::this.scrollListener);
-    window.addEventListener('resize', ::this.scrollListener);
-    this.scrollListener();
-  }
-  detachScrollListener() {
-    window.removeEventListener('scroll', ::this.scrollListener);
-    window.removeEventListener('resize', ::this.scrollListener);
-  }
-  componentWillUnmount() {
-    this.detachScrollListener();
-  }
-}
+import InfiniteList from 'components/InfiniteList';
 
 
 class SimpleListItem extends Component{
@@ -74,7 +19,6 @@ class SimpleListItem extends Component{
     return <div>{this.props.item.type}, {this.props.item.id}</div>
 
     const entry = this.props.collection[this.props.item.id];
-    console.log("YO", entry);
     return <div>{entry.title}</div>
   }
 }
@@ -112,16 +56,16 @@ class PrivateMessagesView extends Component {
     return (
       <div>
         <h1>{private_messages.meta.total} PMs</h1>
-        <InfiniteScrollingList
-          hasMore={private_messages.meta.has_next}
-          loadMore={::this.loadMore}
-          threshold={250}
-          loading={private_messages.isFetching}
+        <InfiniteList
+          meta={private_messages.meta}
+          loader={::this.loadMore}
+          minimalItemHeight={24}
+          isFetching={private_messages.isFetching}
           >
           {private_messages.data.map(x=>
               <SimpleListItem item={x} />
             )}
-        </InfiniteScrollingList>
+        </InfiniteList>
       </div>
     );
   }
---
+++ jsbeavy/components/InfiniteList.jsx
@@ -0,0 +1,119 @@
+import React, { Component, PropTypes} from 'react';
+import Infinite from 'react-infinite';
+import map from 'lodash/collection/map';
+import fill from 'lodash/array/fill';
+
+class SizeReportWrapper extends Component {
+  propTypes: {
+    reportHeight: PropTypes.func.isRequired,
+    element: PropTypes.Component.isRequired,
+  }
+  componentDidMount() {
+    console.log("mounted");
+      var el = React.findDOMNode(this.refs.child);
+      this.props.reportHeight(el.offsetHeight);
+  }
+  render() {
+    console.log("rendering");
+    return React.cloneElement(this.props.element, {ref: "child"});
+  }
+}
+
+export default class InfiniteList extends Component{
+
+  propTypes: {
+    children: PropTypes.array.isRequired,
+    minimalItemHeight: PropTypes.number,
+    loader: PropTypes.func.isRequired,
+    isFetching: PropTypes.bool.isRequired
+    // meta: PropTypes.shape({
+    //   has_next: PropTypes.bool.isRequired,
+    //   page: PropTypes.number.isRequired,
+    //   // "total-pages': PropTypes.number.isRequired,
+    // })
+  }
+
+  constructor(props) {
+      super(props);
+      let minimalItemHeight = props.minimalItemHeight || 100;
+      this.state = {
+          elementHeights: map(props.children, x => minimalItemHeight),
+          containerHeight: window.innerHeight - 60
+      };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+      var propsChanged = this.props !== nextProps;
+      var stateChanged = this.state !== nextState;
+      return propsChanged || stateChanged;
+  }
+
+  render() {
+    console.log(this.state);
+    console.log(map(this.props.children, (c, i) => <SizeReportWrapper element={c} reportHeight={(height) => this.reportHeight(i, height)} />))
+    return <Infinite elementHeight={this.state.elementHeights}
+                     containerHeight={this.state.containerHeight}
+                     infiniteLoadBeginBottomOffset={this.props.meta.has_next && 400}
+                     onInfiniteLoad={::this.handleInfiniteLoad}
+                     loadingSpinnerDelegate={this.elementInfiniteLoad()}
+                     isInfiniteLoading={this.props.isFetching}
+                     preloadBatchSize={20}
+         className="infinite-list"
+         scrollNumberCallback={this.scrollCallback}
+         selectedItem={this.props.selectedItem}
+                     >
+        {map(this.props.children, (c, i) => <SizeReportWrapper element={c} reportHeight={(height) => this.reportHeight(i, height)} />
+        )}
+    </Infinite>;
+  }
+
+  reportHeight(i, height){
+    console.log(i, height);
+    let curHeights = this.state.elementHeights;
+        curHeights[i] = height;
+    this.setState({elementHeights: curHeights});
+  }
+
+  handleResize(e) {
+      this.setState({containerHeight: window.innerHeight - 60});
+  }
+
+  componentDidMount() {
+      window.addEventListener('resize', this.handleResize);
+  }
+
+  componentDidUnmount() {
+      window.removeEventListener('resize', this.handleResize);
+  }
+
+  componentWillReceiveProps(newProps) {
+    let newState = {elements: newProps.children};
+    if (this.props.children.length < newProps.children.length){
+      const minimalItemHeight = newProps.minimalItemHeight || this.props.minimalItemHeight || 100;
+      newState.elementHeights = this.state.elementHeights.concat(
+        fill(Array(newProps.children.length - this.props.children.length), minimalItemHeight))
+    }
+    console.log(newState);
+    this.setState(newState);
+  }
+
+  handleInfiniteLoad() {
+    console.log("LOAD MOA!");
+    this.props.loader(this.props.meta.page + 1)
+  }
+
+  elementInfiniteLoad() {
+      return <div className="infinite-list-item">
+          Loading...
+      </div>;
+  }
+
+  scrollCallback(num) {
+      if(this.props.routeOnScroll) {
+          routeActions.setRoute('/infinite_demo/'+this.props.query+'/'+this.state.elements[num].props.data.masterid);
+      }
+      if (this.props.scrollCallback) {
+          this.props.scrollCallback(num)
+      }
+  }
+}
