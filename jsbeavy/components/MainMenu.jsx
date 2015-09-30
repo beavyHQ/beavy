009cfa6c62bea861d69d99095772d115bf9e2348
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-10-01 00:02:46 +0200

    Display Name in Main Menu

--- jsbeavy/components/MainMenu.jsx
+++ jsbeavy/components/MainMenu.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
 import ReactLogo from "elements/ReactLogo";
+import { NAME } from 'config/config';
 import styles from 'components/MainMenu.scss';
 import classnames from 'classnames';
 import UserMenuWidget from "containers/UserMenuWidget";
@@ -17,6 +18,7 @@ export class MainMenu extends React.Component {
 					  <div className={styles.navigationWrapper}>
 					    <Link to="app" className={styles.logo}>
 					      <img src={logo} alt="Logo Image" />
+					      <span className={styles.title}>{this.props.name || NAME}</span>
 					    </Link>
 					    <a href="" className={styles.navigationMenuButton} id="js-mobile-menu">MENU</a>
 					    <nav role="navigation">
