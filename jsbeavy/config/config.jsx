86662167a9a2c07da8945caba480f05ec530159b
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-10-01 00:02:30 +0200

    grep APP name, too

--- jsbeavy/config/config.jsx
+++ jsbeavy/config/config.jsx
@@ -4,8 +4,9 @@
 // _and_ to the string in the end
 export default Object.assign({
   'MODULES': [],
+  'NAME': 'Nullable',
   'FRONTEND': 'minima',
   'USERS_URL': '/u',
   'ACCOUNT_URL': '/account',
   'SECURITY_REGISTERABLE': true
-}, require("json!grep?MODULES,ACCOUNT_URL,SECURITY_REGISTERABLE,FRONTEND,USERS_URL!yaml!../../config.yml"))
+}, require("json!grep?MODULES,NAME,ACCOUNT_URL,SECURITY_REGISTERABLE,FRONTEND,USERS_URL!yaml!../../config.yml"))
