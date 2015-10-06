c3f9ec657b3232d8622f18e2d8eb1ce7ead6dc70
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-10-06 13:16:43 +0200

    add object and activity default blueprints

--- beavy/blueprints.py
+++ beavy/blueprints.py
@@ -1,9 +1,24 @@
 from flask import Blueprint
 
-users = Blueprint('users', 'beavy.users')
-account = Blueprint('account', 'beavy.account')
+user_bp = Blueprint('users', 'beavy.users')
+account_bp = Blueprint('account', 'beavy.account')
+object_bp = Blueprint('object', 'beavy.object')
+activity_bp = Blueprint('activity', 'beavy.activity')
+
+# Deprecated – Legacy support
+users = user_bp
+account = account_bp
+obj = object_bp
+activity = activity_bp
 
 
 def setup(app):
-    app.register_blueprint(users, url_prefix=app.config.get('USERS_URL', '/u'))
-    app.register_blueprint(account, url_prefix=app.config.get('ACCOUNT_URL', '/account'))
+    app.register_blueprint(user_bp,
+                           url_prefix=app.config.get('USERS_URL', '/u'))
+    app.register_blueprint(account_bp,
+                           url_prefix=app.config.get('ACCOUNT_URL',
+                                                     '/account'))
+    app.register_blueprint(object_bp,
+                           url_prefix=app.config.get('OBJECT_URL', '/o'))
+    app.register_blueprint(activity_bp,
+                           url_prefix=app.config.get('ACTIVITY_URL', '/a'))
