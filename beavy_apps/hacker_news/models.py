ff1ddd58ac9524e7991dabafbe8938ce378209d0
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-10-06 13:50:47 +0200

    starting the infrastructure for the hacker_news backend

---
+++ beavy_apps/hacker_news/models.py
@@ -0,0 +1,27 @@
+from beavy.models.object import Object
+from beavy.common.rendered_text_mixin import RenderedTextMixin
+from beavy.common.payload_property import PayloadProperty
+
+TOPIC_ID = "topic"
+LINK_ID = "link"
+
+
+class Topic(Object, RenderedTextMixin):
+    __mapper_args__ = {
+        'polymorphic_identity': TOPIC_ID
+    }
+
+    title = PayloadProperty('title')
+
+    CAPABILITIES = [Object.Capabilities.listed, Object.Capabilities.searchable]
+
+
+class Link(Object):
+    __mapper_args__ = {
+        'polymorphic_identity': LINK_ID
+    }
+
+    title = PayloadProperty('title')
+    url = PayloadProperty('url')
+
+    CAPABILITIES = [Object.Capabilities.listed, Object.Capabilities.searchable]
