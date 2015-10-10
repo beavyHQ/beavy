d74774f804437ba782c8e4ddad3056b7485e7d2d
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-10-10 23:46:18 +0200

    [hacker news] Add Admin Interface for Link and Topic

--- beavy_apps/hacker_news/__init__.py
+++ beavy_apps/hacker_news/__init__.py
@@ -1,4 +1,7 @@
+from beavy.common.admin_model_view import AdminPolyModelView
+from beavy.app import admin, db
 
+from .models import Link, Topic
 from .blueprint import hn_bp
 from .models import *
 from .views import *
@@ -7,3 +10,11 @@ from .views import *
 def init_app(app):
 
     app.register_blueprint(hn_bp)
+    admin.add_view(AdminPolyModelView(Link, db.session,
+                                      name="Links",
+                                      menu_icon_type='glyph',
+                                      menu_icon_value='glyphicon-resize-full'))
+    admin.add_view(AdminPolyModelView(Topic, db.session,
+                                      name="Topics",
+                                      menu_icon_type='glyph',
+                                      menu_icon_value='glyphicon-file'))
