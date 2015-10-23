c106581ce11d25c3f8eef7834094b8fc3f52ac69
Benjamin Kampmann <ben@create-build-execute.com>
2015-10-23 19:12:21 +0200

    Add Form Submit button testint steps
     - also refactor the rest of the file to remove u'
     - clean up here and there

--- beavy/testing/steps.py
+++ beavy/testing/steps.py
@@ -13,30 +13,45 @@ def go_to_home(context):
 
 @then('I should see the form "{name}"')
 def should_see_form(context, name):
-    assert context.browser.is_element_present_by_css("form[name={}]".format(name)), u'Form {} not found'.format(name)
+    assert context.browser.is_element_present_by_css("form[name={}]".format(name)), 'Form {} not found'.format(name)
 
 
 @then('I should see the form "{name}" within {timeout:d} seconds')
 def should_see_form_with_timeout(context, name, timeout):
-    assert context.browser.is_element_present_by_css("form[name={}]".format(name), wait_time=timeout), u'Form {} not found'.format(name)
+    assert context.browser.is_element_present_by_css("form[name={}]".format(name), wait_time=timeout), 'Form {} not found'.format(name)
 
 
-@step(u'The form "{name}" should be gone')
+@step('The form "{name}" should be gone')
 def should_not_see_form(context, name, timeout):
-    assert context.browser.is_element_not_present_by_css("form[name={}]".format(name)), u'Form {} was found'.format(name)
+    assert context.browser.is_element_not_present_by_css("form[name={}]".format(name)), 'Form {} was found'.format(name)
 
 
-@step(u'The form "{name}" should be gone within {timeout:d} seconds')
+@step('I click the submit button')
+def click_the_submit_button(context, name):
+    el  = context.browser.driver.find_element_by_css_selector("button[type=submit]".format(name))
+    assert el, "Submit button not found"
+    assert el.is_enabled(), "Submit button is disabled"
+    el.click()
+
+@step('I click the submit button of form "{name}"')
+def click_the_submit_button(context, name):
+    el = context.browser.driver.find_element_by_css_selector("form[name={}] button[type=submit]".format(name))
+
+    assert el, "Submit button not found"
+    assert el.is_enabled(), "Submit button is disabled"
+    el.click()
+
+
+@step('The form "{name}" should be gone within {timeout:d} seconds')
 def should_not_see_form_with_timeout(context, name, timeout):
-    assert context.browser.is_element_not_present_by_css("form[name={}]".format(name), wait_time=timeout), u'Form {} was found'.format(name)
+    assert context.browser.is_element_not_present_by_css("form[name={}]".format(name), wait_time=timeout), 'Form {} was found'.format(name)
 
 @given("I am logged in")
 def do_login(context):
     assert context.persona
     user = context.persona
-    USER_LOGIN_FORM = "form[name=login_user_form]"
     if hasattr(context, 'loginButton') and context.loginButton:
-        first_step = 'Click {}"'.format(context.loginButton)
+        first_step = 'I click {}"'.format(context.loginButton)
     else:
         first_step = 'I visit "/login"'
     context.execute_steps("""
@@ -51,5 +66,4 @@ def do_login(context):
         Then the form "login_user_form" should be gone within 5 seconds
     """.format(first_step=first_step,
                email=user.email,
-               form=USER_LOGIN_FORM,
                password="password"))
