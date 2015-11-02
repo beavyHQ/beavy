45ade0c67a5ac9b5bb3be214bf678e53c87de009
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-02 21:55:39 +0100

    split requirements and add external script

---
+++ install.py
@@ -0,0 +1,35 @@
+
+if __name__ == '__main__':
+    import pip
+    import os
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    def install(fn):
+        pip.main(["install", "-r", fn])
+
+    try:
+        import yaml
+    except ImportError:
+        pip.main(["install", "PyYAML"])
+        import yaml
+
+    with open("config.yml") as reader:
+        config = yaml.safe_load(reader)
+
+
+    if os.environ.get("BEAVY_ENV", None):
+        print("----- Install basic requirements")
+        install(os.path.join(BASE_DIR, "beavy", "requirements", "base.txt"))
+        if os.environ("BEAVY_ENV", None) in ["TEST", "DEV", "DEBUG"]:
+            print("----- Install dev requirements")
+            install(os.path.join(BASE_DIR, "beavy", "requirements", "dev.txt"))
+    else:
+        install(os.path.join(BASE_DIR, "beavy", "requirements", "base.txt"))
+        install(os.path.join(BASE_DIR, "beavy", "requirements", "dev.txt"))
+
+    for module in config["MODULES"]:
+        filename = os.path.join(BASE_DIR, "beavy_modules", module, "requirements.txt")
+        if os.path.exists(filename):
+            print("----- Installing Requirements for {}".format(module))
+            install(filename)
