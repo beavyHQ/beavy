d0595c50185a286e219dc0b5ef36bfd62c706486
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-12 15:50:26 +0100

    Lazy import to prevent py.test import circles

--- beavy/common/rate_limits.py
+++ beavy/common/rate_limits.py
@@ -1,7 +1,7 @@
-from beavy.app import limiter, app
 from functools import wraps
 
 def rate_limit(default, *args, **kwargs):
+    from beavy.app import limiter, app
     def inner(fn):
         key = "{}.{}".format(fn.__module__, fn.__name__)
         print(key)
