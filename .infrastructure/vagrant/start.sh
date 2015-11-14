9354abc5635276794088865e12c734b66719a724
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-14 15:05:02 +0100

    add Script to run in vagrant

--- .infrastructure/vagrant/start.sh
+++ .infrastructure/vagrant/start.sh
@@ -9,6 +9,6 @@ tmux select-pane -t 1
 tmux split-window -h
 tmux send-keys "python install.py && python manager.py db upgrade heads && flask --app=main --debug run"  C-m
 tmux select-pane -t 1
-tmux send-keys "npm install && npm run hot-dev-server" C-m
+tmux send-keys "npm install && npm run vagrant" C-m
 tmux select-pane -t 0
 tmux -2 attach-session -t $SESSION
