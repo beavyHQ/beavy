6c5b71654543457e1f48711b9aca25fb3f7f9a6f
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-21 01:31:35 +0100

    Make Session Scrollable for mouse

--- .infrastructure/vagrant/start.sh
+++ .infrastructure/vagrant/start.sh
@@ -3,6 +3,11 @@
 SESSION="beavy"
 
 tmux -2 new-session -d -s $SESSION
+
+tmux set-option -t $SESSION mouse-select-pane on
+tmux set-option -t $SESSION mouse-select-window on
+tmux set-window-option -t $SESSION mode-mouse on
+
 tmux new-window -t $SESSION:1 -n
 tmux split-window -v
 tmux select-pane -t 1
