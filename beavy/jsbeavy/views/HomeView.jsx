f3466fc7c40ef5927f7074c9375732bdff8c46f9
Ethan Zane Miller <ethanzanemiller@gmail.com>
2015-11-22 14:44:32 -0800

    Add logo and link to docs

--- beavy/jsbeavy/views/HomeView.jsx
+++ beavy/jsbeavy/views/HomeView.jsx
@@ -28,7 +28,11 @@ export class HomeView extends React.Component {
   render () {
     return (
       <div className='container text-center'>
+        <img src="http://beavy.xyz/logos/logo.svg" alt="beavy logo" width="150" />
         <h1>Wecome to Beavy!</h1>
+        <p>
+        Please take a look at the <a href="https://beavyhq.gitbooks.io/beavy-documentation/content/" target="_blank">documentation</a>.
+        </p>
       </div>
     );
   }
