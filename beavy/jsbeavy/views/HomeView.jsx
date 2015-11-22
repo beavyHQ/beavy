2ed8fd33062f0e550d6935869387ade8c43a556f
Ethan Zane Miller <ethanzanemiller@gmail.com>
2015-11-22 16:13:30 +0000

    Add home view

--- beavy/jsbeavy/views/HomeView.jsx
+++ beavy/jsbeavy/views/HomeView.jsx
@@ -28,12 +28,7 @@ export class HomeView extends React.Component {
   render () {
     return (
       <div className='container text-center'>
-        <h1>Welcome to the React Redux Starter Kit</h1>
-        <h2>Sample Counter: {this.props.counter}</h2>
-        <button className='btn btn-default'
-                onClick={::this._increment}>
-          Increment
-        </button>
+        <h1>Wecome to Beavy!</h1>
       </div>
     );
   }
