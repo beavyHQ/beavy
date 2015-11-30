149c1f79434874232981d1791a013e9a18c5aa49
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-30 22:05:05 +0700

    Show Browser warnings and errors

--- beavy/testing/environment.py
+++ beavy/testing/environment.py
@@ -3,10 +3,15 @@ from behaving import environment as benv
 from splinter.browser import Browser
 from beavy.app import app
 from .database import ensure_personas, mixer
+from pprint import pprint
 
 import os
+import logging
+
+log = logging.Logger(__name__)
 
 BEHAVE_DEBUG_ON_ERROR = not os.getenv("CI", False)
+BEHAVE_ERROR_ON_BROWSER_WARNINGS = False # os.getenv("BEHAVE_ERROR_ON_BROWSER_WARNINGS", not BEHAVE_DEBUG_ON_ERROR)
 
 def before_all(context):
 
@@ -50,8 +55,22 @@ def before_scenario(context, scenario):
     benv.before_scenario(context, scenario)
     context.personas = ensure_personas()
 
+
 def after_scenario(context, scenario):
+
+    if getattr(context, "browser", None):
+        has_warnings = False
+        for entry in context.browser.driver.get_log('browser'):
+            if entry["level"] in ["WARNING", "ERROR"]:
+                has_warnings = True
+                log.warning("Browser {level}: {timestamp}: {message}".format(**entry))
+
+        if BEHAVE_ERROR_ON_BROWSER_WARNINGS and has_warnings:
+            print("Exciting – Browser Warnings/Errors!")
+            exit(1)
+
     benv.after_scenario(context, scenario)
+
 
 def after_step(context, step):
     if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
@@ -63,7 +82,6 @@ def after_step(context, step):
             import pdb
 
         if getattr(context, "browser", None):
-            from pprint import pprint
             pprint(context.browser.driver.get_log('browser')[-10:])
             print("Current Screen: {}".format(context.browser.screenshot()))
 
