6b3559f8bf90e90f78735ba2c5fce74e9f3a25bd
Benjamin Kampmann <ben@create-build-execute.com>
2015-11-30 18:27:15 +0700

    reattach if the session is already found

--- .infrastructure/vagrant/start.sh
+++ .infrastructure/vagrant/start.sh
@@ -2,18 +2,22 @@
 
 SESSION="beavy"
 
-tmux -2 new-session -d -s $SESSION
+tmux has-session -t $SESSION 2>/dev/null
 
-tmux set-option -t $SESSION mouse-select-pane on
-tmux set-option -t $SESSION mouse-select-window on
-tmux set-window-option -t $SESSION mode-mouse on
+if [ "$?" -eq 1 ] ; then
+  tmux -2 new-session -d -s $SESSION
 
-tmux new-window -t $SESSION:1 -n
-tmux split-window -v
-tmux select-pane -t 1
-tmux split-window -h
-tmux send-keys "python install.py && python manager.py db upgrade heads && flask --app=main --debug run"  C-m
-tmux select-pane -t 1
-tmux send-keys "npm install --all && npm run vagrant" C-m
-tmux select-pane -t 0
+  tmux set-option -t $SESSION mouse-select-pane on
+  tmux set-option -t $SESSION mouse-select-window on
+  tmux set-window-option -t $SESSION mode-mouse on
+
+  tmux new-window -t $SESSION:1 -n
+  tmux split-window -v
+  tmux select-pane -t 1
+  tmux split-window -h
+  tmux send-keys "python install.py && python manager.py db upgrade heads && flask --app=main --debug run"  C-m
+  tmux select-pane -t 1
+  tmux send-keys "npm install --all && npm run vagrant" C-m
+  tmux select-pane -t 0
+fi
 tmux -2 attach-session -t $SESSION
