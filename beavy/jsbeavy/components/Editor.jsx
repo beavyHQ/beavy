9aacba32725f0cf08ad2af730fbbfcb9af5953ab
Benjamin Kampmann <ben@create-build-execute.com>
2015-12-01 19:21:56 +0700

    re-enable prose mirror features – PM is still broken though

--- beavy/jsbeavy/components/Editor.jsx
+++ beavy/jsbeavy/components/Editor.jsx
@@ -1,11 +1,9 @@
-
-
 export default new Promise(resolve => {
   require.ensure([], () => {
-    const ProseMirror = require('react-prosemirror');
-    // require('prosemirror/src/parse/markdown');
-    // require('prosemirror/src/menu/inlinemenu');
-    // require('prosemirror/src/menu/buttonmenu');
-    resolve(ProseMirror);
-  });
-});
+    const ProseMirror = require('react-prosemirror').default
+    require('prosemirror/src/parse/markdown')
+    require('prosemirror/src/serialize/markdown')
+    require('prosemirror/src/menu/menubar')
+    resolve(ProseMirror)
+  })
+})
