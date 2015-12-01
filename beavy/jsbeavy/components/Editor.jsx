00ea537ff805f3ded34840425d89f87e5fdee1d8
Benjamin Kampmann <ben@create-build-execute.com>
2015-12-01 18:51:01 +0700

    disabled features – doesn't work atm

--- beavy/jsbeavy/components/Editor.jsx
+++ beavy/jsbeavy/components/Editor.jsx
@@ -3,9 +3,9 @@
 export default new Promise(resolve => {
   require.ensure([], () => {
     const ProseMirror = require('react-prosemirror');
-    require('prosemirror/src/parse/markdown');
-    require('prosemirror/src/menu/inlinemenu');
-    require('prosemirror/src/menu/buttonmenu');
+    // require('prosemirror/src/parse/markdown');
+    // require('prosemirror/src/menu/inlinemenu');
+    // require('prosemirror/src/menu/buttonmenu');
     resolve(ProseMirror);
   });
 });
