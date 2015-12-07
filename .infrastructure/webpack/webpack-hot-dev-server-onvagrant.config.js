82b9d2a8992b1fe6610a558abeacf9f743f7134d
Benjamin Kampmann <ben@create-build-execute.com>
2015-12-07 12:24:54 +0700

    fix webpack styles

--- .infrastructure/webpack/webpack-hot-dev-server-onvagrant.config.js
+++ .infrastructure/webpack/webpack-hot-dev-server-onvagrant.config.js
@@ -1,13 +1,12 @@
-var config = require("./make-webpack-config")({
+var config = require('./make-webpack-config')({
   devServer: true,
   hotComponents: true,
-  devtool: "#cheap-module-eval-source-map",
+  devtool: '#cheap-module-eval-source-map',
   separateStylesheet: true,
-  // redux_dev_tools: true,
   debug: true,
   watchOptions: {
     poll: true
-  },
-});
-console.log(JSON.stringify(config, null, 2));
-module.exports = config;
+  }
+})
+console.log(JSON.stringify(config, null, 2))
+module.exports = config
