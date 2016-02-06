019a04cc2d0669d3e4bde6f84522047107a95389
Benjamin Kampmann <ben@create-build-execute.com>
2016-02-06 07:26:16 +0000

    Update to latest Prosemirror and React-Prosemirror

--- beavy/jsbeavy/components/Editor.jsx
+++ beavy/jsbeavy/components/Editor.jsx
@@ -1,9 +1,8 @@
 export default new Promise(resolve => {
   require.ensure([], () => {
     const ProseMirror = require('react-prosemirror').default
-    require('prosemirror/src/parse/markdown')
-    require('prosemirror/src/serialize/markdown')
-    require('prosemirror/src/menu/menubar')
+    require('prosemirror/dist/markdown')
+    require('prosemirror/dist/menu/menubar')
     resolve(ProseMirror)
   })
 })
