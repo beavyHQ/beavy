ded3d4ca85bcfd74f332af0ecf5691a0d6578d7a
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-02-24 23:20:31 +0000

    Update server i18n extraction script

--- .infrastructure/i18n/extract-server-msgs.sh
+++ .infrastructure/i18n/extract-server-msgs.sh
@@ -11,6 +11,7 @@ keys="-k format -k format_time -k format_date -k format_datetime"
 # Extraction
 pybabel extract -F babel.cfg $keys -o messages.pot .
 pybabel init -i messages.pot -d . -o './beavy-server.po' -l en
+mkdir var/server-messages
 ./node_modules/.bin/po2json beavy-server.po var/server-messages/beavy-server.json -F -f mf --fallback-to-msgid
 
 # Clean up
