1029c192294fb38cedabbeecaa6dfdcac9a7a075
Benjamin Kampmann <ben@create-build-execute.com>
2016-03-02 09:41:04 +0700

    Fix Wikipedia test case. Fixes #73

--- beavy_modules/url_extractor/lib/fetching_test.py
+++ beavy_modules/url_extractor/lib/fetching_test.py
@@ -132,7 +132,7 @@ def test_wikipedia_example():
             "android-app": "android-app://org.wikipedia/http/en.m.wikipedia.org/wiki/Polygon",
             "application/x-wiki": "/w/index.php?title=Polygon&action=edit"
         },
-        "description": "In elementary geometry, a polygon /\u02c8p\u0252l\u026a\u0261\u0252n/ is a plane figure that is bounded by a finite chain of straight line segments closing in a loop to form a closed chain or circuit. These segments are called its edges or sides, and the points where two edges meet are the polygon's vertices (singular: vertex) or corners. The interior of the polygon is sometimes called its body. An n-gon is a polygon with n sides. A polygon is a 2-dimensional example of the more general polytope in any number of dimensions.",
+        "description": StartsWith("In elementary geometry, a polygon /\u02c8p\u0252l\u026a\u0261\u0252n/ is a plane figure that is bounded by a finite chain of straight line segments closing in a loop to form a closed chain or circuit. These segments are called its edges or sides, and the points where two edges meet are the polygon's vertices (singular: vertex) or corners. The interior of the polygon is sometimes called its body. An n-gon is a polygon with n sides."),
         "generator": StartsWith("MediaWiki"),
         "images": [
             {
