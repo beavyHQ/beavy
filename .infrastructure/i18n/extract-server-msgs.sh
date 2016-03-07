ca654a58774d26f1c5c667807bb404948d405414
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-03-07 09:16:38 +0100

    Add new extraction keys

--- .infrastructure/i18n/extract-server-msgs.sh
+++ .infrastructure/i18n/extract-server-msgs.sh
@@ -4,12 +4,8 @@
 #     `./.infrastructure/i18n/extract-server-messages.sh`
 # Otherwise the working directory will be wrong.
 
-# Keys indicating the fn symbols that pybabel should search for
-# when finding translations.
-keys="-k format -k format_time -k format_date -k format_datetime"
-
 # Extraction
-pybabel extract -F babel.cfg $keys -o messages.pot .
+pybabel extract -F babel.cfg -k format -k format_time -k format_date -k format_datetime -k format_number -k format_currency -k format_decimal -k format_percent -o messages.pot .
 pybabel init -i messages.pot -d . -o './beavy-server.po' -l en
 mkdir var/server-messages
 ./node_modules/.bin/po2json beavy-server.po var/server-messages/beavy-server.json -F -f mf --fallback-to-msgid
