1385a5bcf51c1e3d33a5c2fdbe335a34fa44a3da
Benjamin Kampmann <ben@create-build-execute.com>
2016-03-21 16:45:01 +0100

    improve icu script output

--- .infrastructure/install_icu.sh
+++ .infrastructure/install_icu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 set -e
-
-wget http://download.icu-project.org/files/icu4c/57rc/icu4c-57rc-src.tgz
+echo "Downloading ICU – please wait."
+wget --quiet http://download.icu-project.org/files/icu4c/57rc/icu4c-57rc-src.tgz
 
 # checking md5sum!
 echo 26acb3f79ba926c26bd76094bcf85866 *icu4c-57rc-src.tgz | md5sum -c
 
+echo "Compiling and installing ICU"
 tar xf icu4c-57rc-src.tgz
 cd icu/source
 ./configure prefix=/usr && make && make install
+echo "ICU successfully installed."
