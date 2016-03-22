955722acfdaaf056b03ca1e6b42242dcf0b8fbf5
Benjamin Kampmann <ben@create-build-execute.com>
2016-03-22 10:51:44 +0100

    more explicit checksum function

--- .infrastructure/install_icu.sh
+++ .infrastructure/install_icu.sh
@@ -5,7 +5,7 @@ echo "Downloading ICU – please wait."
 wget --quiet http://download.icu-project.org/files/icu4c/57rc/icu4c-57rc-src.tgz
 
 # checking md5sum!
-echo 26acb3f79ba926c26bd76094bcf85866 *icu4c-57rc-src.tgz | md5sum -c
+echo "26acb3f79ba926c26bd76094bcf85866 *icu4c-57rc-src.tgz" | md5sum -c -
 
 echo "Compiling and installing ICU"
 tar xf icu4c-57rc-src.tgz
