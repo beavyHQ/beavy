48316b4d3f8cd6495031b551b5672640b003764b
Benjamin Kampmann <ben@create-build-execute.com>
2016-03-30 15:35:39 +0200

    Flask Migrate fix: now officially configurable

--- manager.py
+++ manager.py
@@ -1,5 +1,5 @@
 from flask.ext.script import Command, Option
-from flask.ext import migrate as ext_migrate
+from beavy.app import app, manager, migrate
 
 import sys
 import os
@@ -8,31 +8,24 @@ import re
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def patched_migrate(fn):
-    def wrapped(*args, **kwargs):
-        paths = list(filter(lambda x: os.path.isdir(x),
-                     map(lambda x: os.path.join(BASE_DIR,
-                         'beavy_modules', x, "migrations"),
-                            (app.config.get("MODULES", None) or []))))
-        if paths:
-            print("Adding module migrations:\n - {}".format("\n - ".join(paths)))   # noqa
-        app_migrations_path = os.path.join(BASE_DIR,
-                                           'beavy_apps',
-                                           app.config.get("APP"),
-                                           'migrations')
-        if os.path.isdir(app_migrations_path):
-            paths.append(app_migrations_path)
-            print('Adding App migration: \n - {}'.format(app_migrations_path))
-        paths.append(os.path.join('migrations', 'versions'))
-        cfg = fn(*args, **kwargs)
-        cfg.set_main_option('version_locations', " ".join(paths))
-        return cfg
-    return wrapped
-
-
-ext_migrate._get_config = patched_migrate(ext_migrate._get_config)
-
-from beavy.app import app, manager
+@migrate.configure
+def configure_alembic(config):
+    paths = list(filter(lambda x: os.path.isdir(x),
+                 map(lambda x: os.path.join(BASE_DIR,
+                     'beavy_modules', x, "migrations"),
+                        (app.config.get("MODULES", None) or []))))
+    if paths:
+        print("Adding module migrations:\n - {}".format("\n - ".join(paths)))   # noqa
+    app_migrations_path = os.path.join(BASE_DIR,
+                                       'beavy_apps',
+                                       app.config.get("APP"),
+                                       'migrations')
+    if os.path.isdir(app_migrations_path):
+        paths.append(app_migrations_path)
+        print('Adding App migration: \n - {}'.format(app_migrations_path))
+    paths.append(os.path.join('migrations', 'versions'))
+    config.set_main_option('version_locations', " ".join(paths))
+    return config
 
 
 try:
