ce4f8d7d89b867ba8a2ed7e54ee57d7a160d1d48
Benjamin Kampmann <ben@create-build-execute.com>
2016-04-16 18:04:59 +0200

    bumping icu version

--- .infrastructure/install_icu.sh
+++ .infrastructure/install_icu.sh
@@ -4,20 +4,20 @@ set -e
 mkdir -p "$HOME/.icu_cache"
 mkdir -p "$HOME/lib"
 cd "$HOME/.icu_cache"
-if [ ! -d "icu4c-57rc-src" ]; then
+if [ ! -d "icu4c-57_1-src" ]; then
     echo "Downloading latest ICU – please wait."
-    wget --quiet http://download.icu-project.org/files/icu4c/57rc/icu4c-57rc-src.tgz
+    wget --quiet http://download.icu-project.org/files/icu4c/57.1/icu4c-57_1-src.tgz
 
     # checking md5sum!
-    echo "26acb3f79ba926c26bd76094bcf85866 *icu4c-57rc-src.tgz" | md5sum -c -
+    echo "976734806026a4ef8bdd17937c8898b9 *icu4c-57_1-src.tgz" | md5sum -c -
 
-    tar xf icu4c-57rc-src.tgz
-    mv icu icu4c-57rc-src
+    tar xf icu4c-57_1-src.tgz
+    mv icu icu4c-57_1-src
 fi
 
 [ "$TRAVIS" == "true" ] && PREFIX="$HOME/lib" || PREFIX="/usr"
 
 echo "Compiling and installing ICU to $PREFIX"
-cd icu4c-57rc-src/source
-./configure --prefix=$PREFIX && make && make install
+cd icu4c-57_1-src/source
+./configure --prefix=$PREFIX && make &&  make install
 echo "ICU successfully installed."
